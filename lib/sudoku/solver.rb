58af9ae9664bd8d99bc327ceaac11a17d3e358c5
woody2shoes <chuck@lostjungles.com>
2010-11-12 11:51:27 -0600

    the solver works

--- lib/sudoku/solver.rb
+++ lib/sudoku/solver.rb
@@ -9,26 +9,54 @@ module Sudoku
         if board.match(/.*\.txt$/)
           @board = File.open(board).read.split("\n").join('')
         elsif board.is_a?(Array)
-          @board = board.flatten.join('')
+          @board = board.flatten.join
         else
           @board = board
         end
       end
 
       def solve
+        while !solved?
+          pass
+        end
+        disp
+      end
+
+      def pass
+        (1..81).each do |cell|
+          if @board[cell-1, 1].to_i == 0
+            col = [9,1,2,3,4,5,6,7,8][cell%9]
+            row = (cell-1)/9 + 1
+            possibilities = check_cell(row, col)
+            if possibilities.size == 1
+              @board[(cell-1)] = possibilities.first.to_s
+            end
+          end
+        end
+      end
+
+      private
+
+      def disp
+        (1..9).map { |row| @board[(9*(row-1)), 9]}.join("\n")
+      end
+
+      def check_cell(row, col)
+        square = [1,4,7][(row-1)/3] + (col-1)/3
+        check_row(row).select { |i| check_column(col).include?(i) }.select { |i| check_square(square).include?(i) }
       end
 
       def check_row(row_num)
-        raise InvalidRowError, "Row must be between 1 and 9" if row_num <1 || row_num > 9
+        raise InvalidRowError, "Row must be between 1 and 9" if row_num < 1 || row_num > 9
         values = (1..9).to_a
-        @board[row_num-1, 9].each_char do |char|
+        @board[(row_num-1)*9, 9].each_char do |char|
           values.delete_if { |i| i == char.to_i }
         end
         values
       end
 
       def check_column(col_num)
-        raise InvalidColumnError, "Column must be between 1 and 9" if col_num <1 || col_num > 9
+        raise InvalidColumnError, "Column must be between 1 and 9" if col_num < 1 || col_num > 9
         values = (1..9).to_a
         column = []
         (1..9).each do |i|
@@ -40,22 +68,28 @@ module Sudoku
         values
       end
 
-
-      private
-      
-      def get_row
-
+      def check_square(square_num)
+        raise InvalidSquareError, "Square must be between 1 and 9" if square_num < 1 || square_num > 9
+        values = (1..9).to_a
+        square = []
+        row = ((square_num-1)/3) + 1
+        col = [3, 1, 2][square_num%3]
+        rows = (((3*row)-2)..(3*row)).to_a
+        cols = (((3*col)-2)..(3*col)).to_a
+        rows.each do |i|
+          cols.each do |j|
+            square << @board[j - 1 + (9 * (i-1)), 1]
+          end
+        end
+        square.each do |char|
+          values.delete_if { |i| i == char.to_i }
+        end
+        values
       end
 
-      def get_column
-
+      def solved?
+        @board.match(/^[1-9]+$/)
       end
-
-      def get_square
-
-      end
-
-
     end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,20 +6,52 @@ describe Sudoku::Solver::Board do
     @board = Sudoku::Solver::Board.new(File.dirname(__FILE__) + "/test_board.txt")
   end
 
-  it "gets the missing numbers from a row" do
-    @board.check_row(1).should == [1,2,5,7,8]
-  end
+  # I wrote these specs to help me build the solution. However, I have made the supporting methods private, so I'm removing the tests so they don't get run.
+  
+  # it "gets the missing numbers from a row" do
+  #   @board.check_row(4).should == [2,3,4,5,7]
+  # end
 
-  it "throws an error when the row number is invalid" do
-    lambda { @board.check_row(0) }.should raise_error(Sudoku::Solver::InvalidRowError, "Row must be between 1 and 9")
-  end
+  # it "throws an error when the row number is invalid" do
+  #   lambda { @board.check_row(0) }.should raise_error(Sudoku::Solver::InvalidRowError, "Row must be between 1 and 9")
+  # end
 
-  it "gets the missing numbers from a column" do
-    @board.check_column(1).should == [1,2,3,4,6,7]
-  end
+  # it "gets the missing numbers from a column" do
+  #   @board.check_column(1).should == [2,3,4,5,6,7]
+  # end
 
-  it "throws an error when the row number is invalid" do
-    lambda { @board.check_column(10) }.should raise_error(Sudoku::Solver::InvalidColumnError, "Column must be between 1 and 9")
-  end
+  # it "throws an error when the column number is invalid" do
+  #   lambda { @board.check_column(10) }.should raise_error(Sudoku::Solver::InvalidColumnError, "Column must be between 1 and 9")
+  # end
 
+  # it "gets the missing numbers from a square" do
+  #   @board.check_square(1).should == [2,7,9]
+  #   @board.check_square(4).should == [3,5,6,7,8]
+  # end
+
+  # it "throws an error when the square number is invalid" do
+  #   lambda { @board.check_square(10) }.should raise_error(Sudoku::Solver::InvalidSquareError, "Square must be between 1 and 9")
+  # end
+
+  # it "checks for possible values on a cell" do
+  #   @board.check_cell(4, 3).should == [5,7]
+  #   @board.check_cell(6, 7).should == [1,9]
+  # end
+
+  # it "correctly makes a pass on the board" do
+  #   @board.pass
+  #   @board.disp
+  # end
+
+  it "solves the puzzle" do
+    @board.solve.should == "829175463
+763248159
+154693278
+375916842
+941782635
+682534917
+498367521
+236851794
+517429386"
+  end
 end
